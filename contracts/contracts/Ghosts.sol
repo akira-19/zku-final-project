08db6c3f9b45e5022a6112c776db8faba020d7a6
Akira T <akira.tsuruoka.19@gmail.com>
2022-06-23 22:49:15 +0900

    add tests

--- contracts/contracts/Ghosts.sol
+++ contracts/contracts/Ghosts.sol
@@ -10,11 +10,15 @@ contract Ghosts {
     mapping(bytes32 => address) public turnPlayer;
     mapping(bytes32 => uint256) public timeLimit;
     mapping(address => bytes32) public playingGame;
+    mapping(bytes32 => address) public winner;
+
+    // 0: evil, 1: good, 5: hidden, 10: need reveal
+    mapping(bytes32 => mapping(address => uint8[8])) public pieceStatuses;
 
     modifier isMyTurn() {
         bytes32 game = playingGame[msg.sender];
-        require(game != "", "must be playing the game");
-        require(turnPlayer[game] == msg.sender, "must be your turn");
+        require(game != "", "must be playing the game.");
+        require(turnPlayer[game] == msg.sender, "must be your turn.");
         _;
     }
 
@@ -23,30 +27,48 @@ contract Ghosts {
         uint8 toX,
         uint8 toY
     ) {
-        bytes32 game = playingGame[msg.sender];
-        uint8 x = pieces[game][msg.sender][idx][0];
-        uint8 y = pieces[game][msg.sender][idx][1];
+        {
+            bytes32 game = playingGame[msg.sender];
+            uint8 x = pieces[game][msg.sender][idx][0];
+            uint8 y = pieces[game][msg.sender][idx][1];
 
-        int8 dx = int8(toX - x);
-        int8 dy = int8(toY - y);
-        bool isPossibleMove = (abs(dx) == 1 && abs(dy) == 0) ||
-            (abs(dx) == 0 && abs(dy) == 1);
-        bool isNoPieceExist = true;
+            int8 dx = int8(toX) - int8(x);
+            int8 dy = int8(toY) - int8(y);
 
-        for (uint8 i = 0; i < 8; i++) {
-            uint8[2] memory piece = pieces[game][msg.sender][i];
-            if (toX == piece[0] && toY == piece[1]) {
-                isNoPieceExist = false;
-                break;
+            bool isPossibleMove = (abs(dx) == 1 && abs(dy) == 0) ||
+                (abs(dx) == 0 && abs(dy) == 1);
+            bool isNoPieceExist = true;
+
+            for (uint8 i = 0; i < 8; i++) {
+                uint8[2] memory piece = pieces[game][msg.sender][i];
+                if (toX == piece[0] && toY == piece[1]) {
+                    isNoPieceExist = false;
+                    break;
+                }
             }
-        }
 
-        require(isPossibleMove && isNoPieceExist, "must be movable.");
+            require(isPossibleMove && isNoPieceExist, "must be movable.");
+        }
+        _;
+    }
+
+    modifier needReveal() {
+        {
+            bytes32 game = playingGame[msg.sender];
+            bool noNeedToReveal = true;
+            for (uint8 i = 0; i < 8; i++) {
+                if (pieceStatuses[game][msg.sender][i] == 10) {
+                    noNeedToReveal = false;
+                    break;
+                }
+            }
+            require(noNeedToReveal, "need to reveal your piece.");
+        }
         _;
     }
 
     function startGame() public {
-        require(playingGame[msg.sender] == "", "playing another game");
+        require(playingGame[msg.sender] == "", "playing another game.");
         if (waitingGame == "") {
             waitingGame = keccak256(
                 abi.encodePacked(msg.sender, block.timestamp)
@@ -56,12 +78,12 @@ contract Ghosts {
             pieces[waitingGame][msg.sender] = [
                 [1, 4],
                 [2, 4],
-                [1, 5],
-                [2, 5],
                 [3, 4],
                 [4, 4],
+                [1, 5],
+                [2, 5],
                 [3, 5],
-                [3, 5]
+                [4, 5]
             ];
             turnPlayer[waitingGame] = msg.sender;
         } else {
@@ -83,17 +105,77 @@ contract Ghosts {
             ];
             waitingGame = "";
         }
+        for (uint8 i = 0; i < 8; i++) {
+            pieceStatuses[playingGame[msg.sender]][msg.sender][i] = 5;
+        }
     }
 
     function move(
         uint8 idx,
         uint8 toX,
         uint8 toY
-    ) public isMyTurn canMove(idx, toX, toY) {
+    ) public isMyTurn canMove(idx, toX, toY) needReveal {
         pieces[playingGame[msg.sender]][msg.sender][idx] = [toX, toY];
+
+        // check if get the opponent's piece
+        {
+            address opponent;
+            if (players[playingGame[msg.sender]][0] == msg.sender) {
+                opponent = players[playingGame[msg.sender]][1];
+            } else {
+                opponent = players[playingGame[msg.sender]][0];
+            }
+            for (uint8 i = 0; i < 8; i++) {
+                uint8[2] memory piece = pieces[playingGame[msg.sender]][
+                    opponent
+                ][i];
+                if (toX == piece[0] && toY == piece[1]) {
+                    pieces[playingGame[msg.sender]][opponent][i] = [10, 10];
+                    pieceStatuses[playingGame[msg.sender]][opponent][i] = 10;
+                    break;
+                }
+            }
+            turnPlayer[playingGame[msg.sender]] = opponent;
+        }
     }
 
-    function abs(int256 x) private pure returns (int256) {
+    function revealPiece(uint8 idx, uint8 status) public isMyTurn {
+        require(
+            pieceStatuses[playingGame[msg.sender]][msg.sender][idx] == 10,
+            "must be need reveal status."
+        );
+        require(status == 0 || status == 1, "must be zero or one.");
+        pieceStatuses[playingGame[msg.sender]][msg.sender][idx] = status;
+        checkWinnerAfterReveal();
+    }
+
+    function checkWinnerAfterReveal() private {
+        uint8 good = 0;
+        uint8 evil = 0;
+        for (uint8 i = 0; i < 8; i++) {
+            if (pieceStatuses[playingGame[msg.sender]][msg.sender][i] == 0) {
+                evil += 1;
+            } else if (
+                pieceStatuses[playingGame[msg.sender]][msg.sender][i] == 1
+            ) {
+                good += 1;
+            }
+        }
+
+        if (good >= 4) {
+            address opponent;
+            if (players[playingGame[msg.sender]][0] == msg.sender) {
+                opponent = players[playingGame[msg.sender]][1];
+            } else {
+                opponent = players[playingGame[msg.sender]][0];
+            }
+            winner[playingGame[msg.sender]] = opponent;
+        } else if (evil >= 4) {
+            winner[playingGame[msg.sender]] = msg.sender;
+        }
+    }
+
+    function abs(int8 x) private pure returns (int8) {
         return x >= 0 ? x : -x;
     }
 }
--- contracts/test/ghosts-test.js
+++ contracts/test/ghosts-test.js
@@ -21,5 +21,196 @@ describe('Ghosts', function () {
         '0x0000000000000000000000000000000000000000000000000000000000000000',
       );
     });
+
+    it('should join a game', async function () {
+      await ghosts.startGame();
+      await ghosts.connect(player2).startGame();
+      const game = await ghosts.playingGame(player2.address);
+      expect(game).not.equal(
+        '0x0000000000000000000000000000000000000000000000000000000000000000',
+      );
+    });
+  });
+
+  describe('move function', function () {
+    beforeEach(async () => {
+      await ghosts.startGame();
+      await ghosts.connect(player2).startGame();
+    });
+
+    it('should move a piece', async function () {
+      await ghosts.connect(player1).move(0, 1, 3);
+      const game = await ghosts.playingGame(player1.address);
+      const pieceX = await ghosts.pieces(game, player1.address, 0, 0);
+      const pieceY = await ghosts.pieces(game, player1.address, 0, 1);
+      expect(pieceX).to.equal(1);
+      expect(pieceY).to.equal(3);
+    });
+
+    it('should revert moving a piece when not my turn', async function () {
+      await expect(ghosts.connect(player2).move(0, 1, 2)).to.be.revertedWith(
+        'must be your turn.',
+      );
+    });
+
+    it('should revert moving a piece when moving 2 squares', async function () {
+      await expect(ghosts.connect(player1).move(0, 1, 2)).to.be.revertedWith(
+        'must be movable.',
+      );
+    });
+    it('should revert moving a piece when moving to my own piece on it', async function () {
+      await expect(ghosts.connect(player1).move(0, 2, 4)).to.be.revertedWith(
+        'must be movable.',
+      );
+    });
+    it('should revert moving a piece when moving to the same square', async function () {
+      await expect(ghosts.connect(player1).move(0, 1, 4)).to.be.revertedWith(
+        'must be movable.',
+      );
+    });
+  });
+
+  describe('get opponent piece in move function', function () {
+    beforeEach(async () => {
+      await ghosts.startGame();
+      await ghosts.connect(player2).startGame();
+      await ghosts.connect(player1).move(0, 1, 3);
+      await ghosts.connect(player2).move(4, 1, 2);
+      await ghosts.connect(player1).move(0, 1, 2);
+    });
+
+    it('should set piece location is 10, 10', async function () {
+      const game = await ghosts.playingGame(player1.address);
+      const pieceX = await ghosts.pieces(game, player2.address, 4, 0);
+      const pieceY = await ghosts.pieces(game, player2.address, 4, 1);
+      expect(pieceX).to.equal(10);
+      expect(pieceY).to.equal(10);
+    });
+
+    it('should set piece status is 10', async function () {
+      const game = await ghosts.playingGame(player1.address);
+      const status = await ghosts.pieceStatuses(game, player2.address, 4);
+      expect(status).to.equal(10);
+    });
+  });
+
+  describe('move function with need reveal piece', function () {
+    beforeEach(async () => {
+      await ghosts.startGame();
+      await ghosts.connect(player2).startGame();
+      await ghosts.connect(player1).move(0, 1, 3);
+      await ghosts.connect(player2).move(4, 1, 2);
+      await ghosts.connect(player1).move(0, 1, 2);
+    });
+
+    it('should revert moving a piece when moving with need reveal piece', async function () {
+      await expect(ghosts.connect(player2).move(0, 1, 1)).to.be.revertedWith(
+        'need to reveal your piece.',
+      );
+    });
+  });
+
+  describe('revealPiece function', function () {
+    beforeEach(async () => {
+      await ghosts.startGame();
+      await ghosts.connect(player2).startGame();
+      await ghosts.connect(player1).move(0, 1, 3);
+      await ghosts.connect(player2).move(4, 1, 2);
+      await ghosts.connect(player1).move(0, 1, 2);
+    });
+
+    it('should succeed', async function () {
+      const game = await ghosts.playingGame(player1.address);
+      await ghosts.connect(player2).revealPiece(4, 0);
+      const status = await ghosts.pieceStatuses(game, player2.address, 4);
+      expect(status).to.equal(0);
+    });
+
+    it('should fail when not status 10', async function () {
+      await expect(
+        ghosts.connect(player2).revealPiece(3, 0),
+      ).to.be.revertedWith('must be need reveal status.');
+    });
+
+    it('should fail when not 0 or 1', async function () {
+      await expect(
+        ghosts.connect(player2).revealPiece(4, 2),
+      ).to.be.revertedWith('must be zero or one.');
+    });
+
+    describe('checkWinnerAfterReveal function in revealPiece function', function () {
+      describe('get evil ghosts', function () {
+        beforeEach(async () => {
+          await ghosts.connect(player2).revealPiece(4, 0);
+
+          // Player2's all pieces set in front of Player1's
+          await ghosts.connect(player2).move(5, 2, 2);
+          await ghosts.connect(player1).move(1, 2, 3);
+          await ghosts.connect(player2).move(6, 3, 2);
+          await ghosts.connect(player1).move(2, 3, 3);
+          await ghosts.connect(player2).move(7, 4, 2);
+          await ghosts.connect(player1).move(3, 4, 3);
+
+          // Player2 moves not related pieces and Player1 gets Player2's all pieces
+          await ghosts.connect(player2).move(1, 2, 1);
+          await ghosts.connect(player1).move(1, 2, 2);
+          await ghosts.connect(player2).revealPiece(5, 0);
+
+          await ghosts.connect(player2).move(1, 2, 0);
+          await ghosts.connect(player1).move(2, 3, 2);
+          await ghosts.connect(player2).revealPiece(6, 0);
+
+          await ghosts.connect(player2).move(1, 2, 1);
+          await ghosts.connect(player1).move(3, 4, 2);
+          await ghosts.connect(player2).revealPiece(7, 0);
+        });
+        it('should set winner player2', async function () {
+          const game = await ghosts.playingGame(player1.address);
+          const winner = await ghosts.winner(game);
+          expect(winner).to.equal(player2.address);
+        });
+      });
+      describe('get good ghosts', function () {
+        beforeEach(async () => {
+          await ghosts.connect(player2).revealPiece(4, 1);
+
+          // Player2's all pieces set in front of Player1's
+          await ghosts.connect(player2).move(5, 2, 2);
+          await ghosts.connect(player1).move(1, 2, 3);
+          await ghosts.connect(player2).move(6, 3, 2);
+          await ghosts.connect(player1).move(2, 3, 3);
+          await ghosts.connect(player2).move(7, 4, 2);
+          await ghosts.connect(player1).move(3, 4, 3);
+
+          // Player2 moves not related pieces and Player1 gets Player2's all pieces
+          await ghosts.connect(player2).move(1, 2, 1);
+          await ghosts.connect(player1).move(1, 2, 2);
+          await ghosts.connect(player2).revealPiece(5, 1);
+
+          await ghosts.connect(player2).move(1, 2, 0);
+          await ghosts.connect(player1).move(2, 3, 2);
+          await ghosts.connect(player2).revealPiece(6, 1);
+
+          await ghosts.connect(player2).move(1, 2, 1);
+          await ghosts.connect(player1).move(3, 4, 2);
+          await ghosts.connect(player2).revealPiece(7, 1);
+        });
+        it('should set winner player2', async function () {
+          const game = await ghosts.playingGame(player1.address);
+          const winner = await ghosts.winner(game);
+          expect(winner).to.equal(player1.address);
+        });
+      });
+    });
   });
 });
+
+// function revealPiece(uint8 idx, uint8 status) public isMyTurn {
+//   require(
+//       pieceStatuses[playingGame[msg.sender]][msg.sender][idx] == 10,
+//       "must be need reveal status."
+//   );
+//   require(status == 0 || status == 1, "must be zero or one.");
+//   pieceStatuses[playingGame[msg.sender]][msg.sender][idx] = status;
+//   checkWinnerAfterReveal();
+// }
