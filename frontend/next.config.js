151af7f9a04de59ceb1ffa90ffb5938f1c6261d1
Akira T <akira.tsuruoka.19@gmail.com>
2022-07-03 21:25:17 +0900

    modify config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,13 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.experiments = config.experiments || {}
-    config.experiments.topLevelAwait = true
-    config.resolve = config.resolve || {}
-    config.resolve.fallback = config.resolve.fallback || {}
+    config.experiments = config.experiments || {};
+    config.experiments.topLevelAwait = true;
+    config.resolve = config.resolve || {};
+    config.resolve.fallback = config.resolve.fallback || {};
     config.resolve.fallback.fs = false;
-    return config
-  },
-}
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename = 'public/Ghosts.wasm';
 
-module.exports = nextConfig
+    return config;
+  },
+};
