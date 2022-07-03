edf0cf66e5b8708e250fec1c01071c9ba36600d1
Akira T <akira.tsuruoka.19@gmail.com>
2022-07-03 22:00:04 +0900

    change config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
+  webpack: (config, options) => {
     config.experiments = config.experiments || {};
     config.experiments.topLevelAwait = true;
     config.resolve = config.resolve || {};
     config.resolve.fallback = config.resolve.fallback || {};
-    if (!isServer) {
+    if (!options.isServer) {
       config.resolve.fallback.fs = false;
     }
+    config.experiments = { asyncWebAssembly: true };
 
     return config;
   },
